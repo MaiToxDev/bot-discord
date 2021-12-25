f2c7b309b345599265210eb68ad318ffc7b31881
MaiToxDev <93232153+MaiToxDev@users.noreply.github.com>
2021-12-25 05:46:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const { channel } = require("diagnostics_channel");
+var Discord = require("discord.js");
+var Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const prefix = "*";
+
+Client.on("ready", () => {
+    console.log("BOT Opérationnel");
+});
+
+
+// Client.on("guildMemberAdd", member => {
+//     console.log("Un nouveau membre est arrivé");
+//     member.guild.channels.cache.find(channel => channel.id === "919639759712833559").send(member.displayName + " est arrivé !\nNous sommes désormais **" + member.guild.memberCount + "** sur le serveur !!");
+// });
+
+
+Client.on("messageCreate", message => {
+    if(message.author.bot) return;
+
+    //!!!ping
+    if(message.content === prefix + "ping"){
+        message.reply("Pong !")
+    }
+
+    //!!!help
+    else if(message.content === prefix + "help"){
+        const embed = new Discord.MessageEmbed()
+            .setColor("GOLD")
+            .setTitle("Liste des commandes")
+            .setAuthor("_MaiTox_#8776")
+            .setDescription("**Voici la liste des commandes de mon BOT:**")
+            .addField("*help", "Affiche la liste des commandes")
+            .addField("*ping", "Vous renvoie pong")
+            .setTimestamp()
+            .setFooter("BOT crée par _MaiTox_#8776");
+
+        message.channel.send({ embeds: [embed]});
+    }
+
+});
+
+Client.login(process.env.TOKEN);
